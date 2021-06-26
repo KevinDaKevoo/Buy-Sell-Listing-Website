0247d28727ca13c394200f645bd28da3e96bc359
Kevin <kevin.yu.kfy@gmail.com>
2021-06-26 23:53:29 +0000

    removed scripts folder and add express.router to admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,22 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
+
 
 
 
@@ -20,5 +37,9 @@ app.post("/admin/product_id/delete", (res, req) => {
 });
 
 app.post("/admin/product_id/sold", (res, req) => {
+
+});
+
+app.get("/logout", (res, req) => {
 
 });
