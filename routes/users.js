1305325bdc3cb4dbcb3a9881b12a524985cf49f1
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-29 01:04:53 +0000

    implement login feature and cookie session, update header.js file with js logic

--- routes/users.js
+++ routes/users.js
@@ -5,63 +5,53 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
-      .then(data => {
+      .then((data) => {
         const users = data.rows;
         const current_user = req.session.user_email;
         res.json({ users });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });      
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
   });
 
   router.get("/login", (req, res) => {
-    console.log('entering route:');
+    console.log("entering route:");
     db.query(`SELECT * FROM users;`)
-      .then(data => {
+      .then((data) => {
         const users = data.rows;
         const user_email = req.session.user_email;
         const templateVars = { user_email };
         return res.render("login", templateVars);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({error: err.message});
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
-    
   });
-  
+
   router.post("/login", (req, res) => {
     const email = req.body.email;
     const values = [email];
     const sqlQuery = `SELECT * FROM users WHERE email = $1`;
-    return db.query(sqlQuery, values)
-      .then(data => {
+    return db
+      .query(sqlQuery, values)
+      .then((data) => {
         const user = data.rows[0];
         if (user) {
-          console.log("I am here");
           req.session.user_email = user.email;
-          console.log(req.session.user_email);
-
-          res.redirect("/"); //may be res.render("index") - try it now?
+          res.redirect("/");
         } else {
           res.send("Unauth access");
         }
-      }) //maybe in /login get needs res.session.user_email?
-      .catch(err => {
-        res
-          .status(500)
-          .json({err: err.message});
+      }) 
+      .catch((err) => {
+        res.status(500).json({ err: err.message });
       });
 
     // return res.redirect("/");
@@ -71,10 +61,9 @@ module.exports = (db) => {
     req.session = null;
     res.redirect("/");
   });
-  
+
   return router;
 };
-
 
 /*
 router.get("/product/:product_id", (res, req) => {
