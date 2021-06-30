07b0c8065fb7e31d9d2b6ced55526d4e4727aa0e
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-30 16:10:05 +0000

    implemented message route, implement and modify message page and new message page, modified products page to display seller id and contact seller button

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, email, phone, city, is_admin) VALUES ('Ally', 'ally@123.com', 416999123, 'Toronto', True);
 INSERT INTO users (name, email, phone, city, is_admin) VALUES ('Kira', 'kira@gmail.com', 123456789, 'Montreal', False);
+INSERT INTO users (name, email, phone, city, is_admin) VALUES ('Rex', 'rex@gmail.com', 1112223334, 'Vancouver', False);
 
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,19 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+
+$(document).ready(function () {
+  $("#reply-message-box").hide();
+
+  $("#reply-message-button").click(function (event) {
+    event.preventDefault();
+    $("#reply-message-box").show();
+  });
 });
